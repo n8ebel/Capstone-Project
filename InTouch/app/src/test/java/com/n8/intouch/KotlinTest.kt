711a1fd8ecd4b7609ab3b8bcad4e9ace77e3357b
n8ebel <nebel@vandals.uidaho.edu>
2015-11-29 22:25:12 -0800

    Added a couple basic test class stubs

---
+++ InTouch/app/src/test/java/com/n8/intouch/KotlinTest.kt
@@ -0,0 +1,7 @@
+package com.n8.intouch
+
+/**
+ * Created by n8 on 11/28/15.
+ */
+class KotlinTest {
+}
---
+++ InTouch/app/src/test/java/com/n8/intouch/main/MainInteractorImplTest.kt
@@ -0,0 +1,22 @@
+package com.n8.intouch.main
+
+import junit.framework.TestCase
+
+/**
+ * Created by n8 on 11/28/15.
+ */
+class MainInteractorImplTest : TestCase() {
+
+    public override fun setUp() {
+        super.setUp()
+
+    }
+
+    public override fun tearDown() {
+
+    }
+
+    fun testGetData() {
+
+    }
+}
